ca4be583e224ce3bfcb1f468ea5cf9f0c06f86b4
Oliver Coldwell <olivercoldwell@gmail.com>
2021-05-25 00:52:41 +0100

    refactored and organised code into similar functional blocks

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,6 @@
 const container = document.getElementById("grid-container");
 const buttonContainer = document.getElementById("button-container");
 
-const buttonState= {
-  'rainbow': false,
-  'default': true,
-  'pen': false,
-  'erase': false
-}
-const toRGBA=(red, green, blue, alpha = 1.0)=> {
-  return `rgba(${red}, ${green}, ${blue}, ${alpha})`
-}
-
-const randomRGB = () => {
-    return Math.floor(Math.random() * 256);
-  };
-
-const rainbowEtch = (e) => {
-  e.target.style.backgroundColor = toRGBA(randomRGB(),randomRGB(), randomRGB());
-};
-
 const createGrid = (size) => {
   size = size || 16;
   const gridSize = size * size;
@@ -37,24 +19,28 @@ const sizeOfGrid = () => {
   let size = Number(prompt("Enter size here (no greater than 100): "));
   createGrid(size);
 };
+
 const resetGrid = () => {
   let cells = document.querySelectorAll(".cell");
   cells.forEach((cell) => cell.remove());
   createGrid();
 };
 
-const drawColor = (e) => {
- if (buttonState.default) {
-   defaultEtch(e);
- } else if (buttonState.rainbow) {
-   rainbowEtch(e)
- } else if (buttonState.pen) {
-   penEtch(e);
- } else if (buttonState.erase) {
-   eraseEtch(e)
- }
- }
+const toRGBA = (red, green, blue, alpha = 1.0) => {
+  return `rgba(${red}, ${green}, ${blue}, ${alpha})`;
+};
 
+const randomRGB = () => {
+  return Math.floor(Math.random() * 256);
+};
+
+const rainbowEtch = (e) => {
+  e.target.style.backgroundColor = toRGBA(
+    randomRGB(),
+    randomRGB(),
+    randomRGB()
+  );
+};
 
 const defaultEtch = (e) => {
   e.target.style.backgroundColor = "black";
@@ -63,17 +49,36 @@ const defaultEtch = (e) => {
 const eraseEtch = (e) => {
   e.target.style.backgroundColor = container.style.backgroundColor;
 };
-// This handles the value to be entered on each button press which allows for an extensible handler.
+
+const drawColor = (e) => {
+  if (buttonState.default) {
+    defaultEtch(e);
+  } else if (buttonState.rainbow) {
+    rainbowEtch(e);
+  } else if (buttonState.pen) {
+    penEtch(e);
+  } else if (buttonState.erase) {
+    eraseEtch(e);
+  }
+};
+
+const buttonState = {
+  rainbow: false,
+  default: true,
+  pen: false,
+  erase: false,
+};
+
 const buttonStateHandle = (e) => {
   for (const [key] of Object.entries(buttonState)) {
     if (key != e.target.value) {
-     buttonState[key] = false;
+      buttonState[key] = false;
     } else {
-     buttonState[key] = true;
+      buttonState[key] = true;
     }
   }
-}
-//Click Handler using inheritance.
+};
+
 const handleClick = (e) => {
   switch (e.target.value) {
     case "reset":
@@ -96,5 +101,6 @@ const handleClick = (e) => {
       break;
   }
 };
+
 buttonContainer.addEventListener("click", handleClick);
 createGrid();
